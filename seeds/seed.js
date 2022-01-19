7756c4e6d3adebd503423cefb7a7b2b4619bb339
Paper Girl <jamie.rose.528@gmail.com>
2022-01-18 21:24:31 -0800

    seeds

---
+++ seeds/seed.js
@@ -0,0 +1,21 @@
+const sequelize = require("../config/connection");
+const { User, Post } = require("../models");
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const user = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+  for (const post of postData) {
+    await Post.create({
+      ...post,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
